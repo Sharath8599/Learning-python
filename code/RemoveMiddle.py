9add381dea15f6b1d4dcd1069f366527480c9ec9
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 21:43:17 -0600

    Create RemoveMiddle.py

---
+++ code/RemoveMiddle.py
@@ -0,0 +1,10 @@
+# Create a function named remove_middle which has three parameters named my_list, start, and end.
+
+# The function should return a list where all elements in my_list with an index between start and end (inclusive) have been removed.
+
+
+def remove_middle(my_list, start, end):
+  return my_list[:start]+my_list[end+1:]
+
+
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
