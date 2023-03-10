6db8a2cfa622b1bdb5b79e5f34f63bbfd31ea14c
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 23:29:25 -0600

    Create BondJamesBond.py

---
+++ code/BondJamesBond.py
@@ -0,0 +1,11 @@
+# Write a function named introduction() that has two parameters named first_name and last_name.
+
+#The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
+
+def introduction(first_name, last_name):
+  return last_name+", "+first_name+" "+last_name
+
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
