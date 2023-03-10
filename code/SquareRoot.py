2106afc63f14aa8991c888b6849a3ec0f2d2e0e0
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 23:16:56 -0600

    Create SquareRoot.py

---
+++ code/SquareRoot.py
@@ -0,0 +1,11 @@
+# Write a function named square_root() that has one parameter named num.
+
+#Use exponents (**) to return the square root of num.
+
+def square_root(num):
+  return num**0.5
+
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
