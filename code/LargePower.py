dc3f5047c46610e697cc2eff5850fdb98d6619c1
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 20:23:04 -0600

    Update LargePower.py

--- code/LargePower.py
+++ code/LargePower.py
@@ -1,4 +1,6 @@
-# Write your large_power function here:
+# Create a function named large_power() that takes two parameters named base and exponent.
+# If base raised to the exponent is greater than 5000, return True, otherwise return False
+
 def large_power(base,exponent):
   power=base**exponent
   if power>5000:
