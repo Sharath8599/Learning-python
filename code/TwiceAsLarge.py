47eae1a15c97977a57a5b5adc78b31ee2e90fa11
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 20:32:20 -0600

    Update TwiceAsLarge.py

--- code/TwiceAsLarge.py
+++ code/TwiceAsLarge.py
@@ -8,7 +8,6 @@ def twice_as_large(num1, num2):
   else:
     return False
 
-# Uncomment these function calls to test your twice_as_large function:
 print(twice_as_large(10, 5))
 # should print False
 print(twice_as_large(11, 5))
