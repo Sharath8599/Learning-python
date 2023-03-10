081600274e54d16632c3f5749c4412720c388662
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 20:31:09 -0600

    Update OverBudget.py

--- code/OverBudget.py
+++ code/OverBudget.py
@@ -9,7 +9,7 @@ def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
   else:
     return False
 
-# Uncomment these function calls to test your over_budget function:
+
 print(over_budget(100, 20, 30, 10, 40))
 # should print False
 print(over_budget(80, 20, 30, 10, 30))
