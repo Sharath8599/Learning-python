75261dcdc27cea51e76a70de09a344c3c2d4ed5a
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 23:27:34 -0600

    Create Tip.py

---
+++ code/Tip.py
@@ -0,0 +1,12 @@
+# Create a function called tip() that has two parameters named total and percentage.
+
+#This function should return the amount you should tip given a total and the percentage you want to tip.
+
+
+def tip(total,percentage):
+  return (total*percentage)/100
+
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
