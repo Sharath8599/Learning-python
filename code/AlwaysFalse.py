f6be4832fccc40281888320f36fb47679f441d41
Sharath Prathapagiri <39335456+Sharath8599@users.noreply.github.com>
2023-03-09 20:45:31 -0600

    Create AlwaysFalse.py

---
+++ code/AlwaysFalse.py
@@ -0,0 +1,14 @@
+# Create a function named always_false() that has one parameter named num.
+
+#Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num.
+
+def always_false(num):
+  if num>=0 or num<=0:
+    return False
+
+print(always_false(0))
+# should print False
+print(always_false(-1))
+# should print False
+print(always_false(1))
+# should print False
